1e1b751f7ecb81fefc2b19e188e7a13002997220
tuanngoc.n <tuanngoccode4fun@gmail.com>
2020-06-17 21:50:50 +0700

    Add project files.

---
+++ Queue algorithm/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Buffer buf = new Buffer();
+            buf.SendMessage(new Message("One"));
+            buf.SendMessage(new Message("Two"));
+            buf.ReceiveMessage();
+            buf.SendMessage(new Message("Three"));
+            buf.ReceiveMessage();
+            buf.SendMessage(new Message("Four"));
+            buf.ReceiveMessage();
+            buf.ReceiveMessage();
+        }
+    }
+
+        class Message
+        {
+            private string messageText;
+            public Message(string s)
+            {
+                messageText = s;
+            }
+            public override string ToString()
+            {
+                return messageText;
+            }
+        }
+        class Buffer
+        {
+            private Queue messageBuffer;
+            public void SendMessage(Message m)
+            {
+                messageBuffer.Enqueue(m);
+                Console.WriteLine("send "+ m.ToString());
+                Console.ReadKey();
+            }
+            public void ReceiveMessage()
+            {
+                Message m = (Message)messageBuffer.Dequeue();
+                Console.WriteLine("recived "+ m.ToString());
+                Console.ReadKey();
+             }
+            public Buffer()
+            {
+                messageBuffer = new Queue();
+            }
+
+        }
+
+       
+
+}
